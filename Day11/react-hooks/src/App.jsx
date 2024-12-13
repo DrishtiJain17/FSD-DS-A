a2a9177987f655cdb56d71712484f10bb362f393
Drishti Jain <drishtijain1704@gmail.com>
2024-12-13 09:19:12 +0530

    added dashboard to day 11

--- Day11/react-hooks/src/App.jsx
+++ Day11/react-hooks/src/App.jsx
@@ -1,25 +1,25 @@
-import {BrowserRouter,Routes,Route} from "react-router-dom";
-import MainLayout from './components/MainLayout';
-import Register from './components/Register';
-import Login from "./components/login";
-import Dashboard from "./components/dashboard";
-import Logout from "./components/logout";
-import { useState } from "react";
-
-
+import React, { useState } from 'react'
+import {BrowserRouter,Routes,Router,Route} from "react-router-dom"
+import MainLayout from './components/MainLayout'
+import Register from './components/Register'
+import Login from './components/Login'
+import DashBoard from './components/dashboard'
+import Logout from './components/Logout'
 const App = () => {
   const [data,setData] = useState();
   return (
     <div>
       {JSON.stringify(data)}
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<MainLayout/>}/>
-          <Route path="/register" element={<Register regData={setData}/>}/>
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/dashboard" element={<Dashboard/>}/>
-          <Route path="/logout" element={<Logout/>}/>
-        </Routes>
+      <Routes>
+        <Route path='/' element={<MainLayout/>}>
+          <Route path='/login' element={<Login regLogin={data}/>}/>
+          <Route path='/register' element={<Register regData={setData}/>}/>
+        </Route>
+        <Route path='/dashboard' element={<DashBoard regDash={data}/>}/>
+        <Route path='/logout' element={<Logout/>}/>
+
+      </Routes>
       </BrowserRouter>
     </div>
   )
--- Day11/react-hooks/src/components/Dashboard.jsx
+++ Day11/react-hooks/src/components/Dashboard.jsx
@@ -1,19 +1,36 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-const DashBoard = ({ regDash }) => {
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Dashboard = ({regDash}) => {
   return (
     <div>
+      <div>
       <div className="container">
-        <div className="jumbotron">
-          <h1>User DashBoard</h1>
-          <h2>Logout</h2>
-          <p>Welcome {regDash.name}</p>
+        <div className="jumbotron"
+        style={{color:"white", backgroundColor:"darkkhaki" }}>
+          <div className='row'>
+            <div className='col-sm-12'>
+              <h1>User Dashboard</h1>
+            </div>
+          </div>
+          <div className='row'>
+            <div className='col-sm-8'>
+              <h2>Welcome {regDash.name}</h2>
+            </div>
+          </div>
+          <div className='col-sm-4'>
+            <h1>
+              <Link to="/logout">Logout</Link>
+            </h1>
+          </div>
         </div>
         <p>You Login Email : {regDash.email}</p>
         <p>Click the button to Fetch Weather</p>
       </div>
     </div>
-  );
-};
 
-export default DashBoard;
+    </div>
+  )
+}
+
+export default Dashboard
